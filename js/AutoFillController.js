a56eb89bce4d681815a4ddf0ad9155bbd5c8f849
calvin <calvincrane@gmail.com>
2014-02-07 21:37:10 +0000

    got the worst issues behind now may be less than efficent at thsi stage

--- js/AutoFillController.js
+++ js/AutoFillController.js
@@ -2,59 +2,49 @@ var autoFillControllers=angular.module('autoFillControllers',[]);
 
 autoFillControllers.controller('AutoFill',function($scope,$firebase){
 
-	$scope.countries = new Firebase("https://landed.firebaseio.com/places/");
+	var fb = new Firebase("https://landed.firebaseio.com/places");
+
+	$scope.countries = $firebase( fb );
+
+
 	$scope.suggest='';
 	$scope.matches=[];
+
+	$scope.countries.$bind($scope, "SYNC");
 
     /*var countries = new Firebase("https://landed.firebaseio.com/places/");
    	var pushRef = countries.push();// this allows us to remove a specific one by ref later like pushRef.remove();
    	pushRef.set({'name':'Lincoln'}); // or directly use push() !
    	*/
 
-   $scope.addPlace= function(pl,type){
-   	console.log('addPlace called '+pl + $scope.place+ type);
-   	var BreakException= {};
-   	if(pl && !type){
-   		
-   		//need to check for existing entry in case the user just hits enter or other way.
-			$scope.countries.once('value',function(placeSnapshot){
+	$scope.addPlace= function(pl,type){
 
-			try{
+		fb.once('value', function(snap) { 
 
-				placeSnapshot.forEach(function(leSnapshot) {
-					var name = leSnapshot.child('name/').val();
-					name=angular.lowercase(name);
-					if(name == pl){
-						console.log('trying to submit duplicate for '+ pl);
-						throw BreakException;
-						
-					}else{
-						console.log(pl+ 'not currently in database');
-						$scope.countries.push({'name':pl});
-						throw BreakException;
-					}
-				});
+   			if( snap.val() === null ) {}
+   				else{
+   					
+   					var keys = $scope.SYNC.$getIndex();
+   					var dupes=false;
 
-			}catch(e){
-				if (e!==BreakException) throw e;
-			};
+   						keys.forEach(function(key, i) {
+						 	
+						 	//console.log("comparing "+$scope.countries[key]['placeName'] +" to "+pl); // prints items in order they appear in Firebase
+							
+							if($scope.countries[key]['placeName'] == pl){
+								console.log('trying to submit duplicate for '+ pl);
+								dupes=true;
+							}
+						 });
 
-			});
-   		
-   		
-   	}
-   	else{
-   		//we are suggesting so just populate the value in the input
-   		//console.log('from the click on field '+$scope.suggest);
-   		$scope.needed=pl;
-   		//$scope.matches=[];
-
-   	}
-   }
+   						if(!dupes) $scope.countries.$add( {'placeName':pl} );
+   				}
+		});
+	}
 
 
 	$scope.changeField = function(str){
-
+//return false;
 		//string changes length each time this function runs and it can get smaller !
 		var aMatches=[];
 		//console.log("new letter "+str+" .. "+aMatches.toString());
@@ -62,16 +52,16 @@ autoFillControllers.controller('AutoFill',function($scope,$firebase){
 			str = angular.lowercase(str); //using angular lowercase could use plain javascript too.
 			var newStringLength = str.length; //need to loop through this length times..
 
-				$scope.countries.once('value',function(placeSnapshot){ // we cant have this being called each interaction for each character loop for sure..!
+				fb.once('value',function(placeSnapshot){ // we cant have this being called each interaction for each character loop for sure..!
 					
 					for (var i=0; i<newStringLength;i++){
 						//console.log("index is "+ i +" character at " + str.charAt(i));
 
 						placeSnapshot.forEach(function(leSnapshot) {
-				
-	  								var name = leSnapshot.child('name/').val();//eg Lisbon then L.A. then New York (for each place already in the firebase)
+				//console.log(leSnapshot.val());
+	  								var name = leSnapshot.child('placeName').val();//eg Lisbon then L.A. then New York (for each place already in the firebase)
 									name=angular.lowercase(name);//aNames.push(name);
-									//console.log(i);
+									//console.log(name);
 						  			//if(str.indexOf(name.charAt(i))!= -1)
 						  			//console.log("checking "+str.charAt(i)+" against "+name.charAt(i));
 						  			//if(str.charAt(i) == name.charAt(i))// this was one of the bigger errors for achieving this as it subsequently forgets the preceding characters
@@ -90,14 +80,12 @@ autoFillControllers.controller('AutoFill',function($scope,$firebase){
 					//show final matches...
 					$scope.matches=[];
 					$scope.matches=_.uniq(aMatches,false);
-					console.log($scope.matches);
+					//console.log($scope.matches);
 
 				});
 		}
 
 	};
-
-
 
 
 });
